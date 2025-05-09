3fa31f39c9f23da7663ee1507883cb3f6732ce21
karolk95 <66304596+karolk95@users.noreply.github.com>
2025-05-09 22:54:40 +0200

    Add files via upload

---
+++ cookiebot-for-ghl.js
@@ -0,0 +1,10 @@
+<script>
+    (function() {
+        var script = document.createElement('script');
+        script.id = "Cookiebot";
+        script.src = "https://consent.cookiebot.com/uc.js";
+        script.type = "text/javascript";
+        script.setAttribute("data-cbid", "xxx-xxx-xxx");
+        document.head.appendChild(script);
+    })();
+</script>
